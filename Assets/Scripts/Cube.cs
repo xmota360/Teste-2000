b58ec4be5f0a8014ebf78760524a8677af90335e
xmota360 <aluno@S08PC02.local>
2016-05-04 19:18:27 +0100

    Cube Translation Script

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cube : MonoBehaviour 
+{
+	public float lerpFactor = 0f;
+	public float startPositionX = 0f;
+	public float endPositionX = 5f;
+
+	private Vector3 currentPosition = new Vector3 ();
+	private float destination = 5f;
+
+
+	void Start () 
+	{
+	}
+
+	void Update () 
+	{
+		lerpFactor += Time.deltaTime;
+
+		if (transform.position.x != destination) 
+		{
+			currentPosition.x = Mathf.Lerp (currentPosition.x, destination, lerpFactor);
+			transform.position = currentPosition;
+		} 
+		else 
+		{
+			if (transform.position.x == startPositionX)
+				destination = endPositionX;
+			else
+				destination = startPositionX;
+
+			lerpFactor = 0f;
+		}
+	}
+}
